cd41b6c7d567bedd7231b3cab5a77d3c5737b0bb
yanamikha <yanamikha@gmail.com>
2025-02-26 01:42:53 +0100

    remove FontAwesomeIcon import

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -32,7 +32,6 @@ function convertMs(ms) {
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
   const days = addLeadingZero(Math.floor(ms / day));
   const hours = addLeadingZero(Math.floor((ms % day) / hour));
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
